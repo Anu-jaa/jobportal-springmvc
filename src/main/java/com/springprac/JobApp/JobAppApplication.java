141dc1c4e955fc72c8af8898e6b8a8de81a2d026
Anuja <anuja.minde@gmail.com>
2025-08-03 17:07:16 +0530

    Initial commit: Spring MVC JobPortal setup

---
+++ src/main/java/com/springprac/JobApp/JobAppApplication.java
@@ -0,0 +1,16 @@
+package com.springprac.JobApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class JobAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JobAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springprac/JobApp/JobController.java
@@ -0,0 +1,14 @@
+package com.springprac.JobApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+
+    @RequestMapping({"/","home"})
+    public String home(){
+        System.out.println("Returning home page..");
+        return "home";
+    }
+}
